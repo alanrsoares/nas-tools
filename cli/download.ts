cf84e4ea76a1fc23abd6522f4b659c752db09289
Alan R. Soares <alanrsoars@gmail.com>
2025-08-15 13:42:12 +1200

    chore: use undici

--- cli/download.ts
+++ cli/download.ts
@@ -1,8 +1,7 @@
 import { mkdir, writeFile } from "node:fs/promises";
 import { basename } from "node:path";
 import { Command } from "commander";
-
-import { fetch } from "bun";
+import { fetch } from "undici";
 
 const DEFAULT_DEST = "/volmain/Download/ignore";
 const DEFAULT_UA =
