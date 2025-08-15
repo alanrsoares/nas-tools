7f1887380b9eba35b70c3596036ade164631419b
Alan R. Soares <alanrsoars@gmail.com>
2025-08-15 13:42:50 +1200

    fix: type mismatch

--- cli/download.ts
+++ cli/download.ts
@@ -1,7 +1,7 @@
 import { mkdir, writeFile } from "node:fs/promises";
 import { basename } from "node:path";
 import { Command } from "commander";
-import { fetch } from "undici";
+import { fetch, Response } from "undici";
 
 const DEFAULT_DEST = "/volmain/Download/ignore";
 const DEFAULT_UA =
